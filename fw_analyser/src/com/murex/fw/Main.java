a194176020677b055a27e5c8bf0924406cff5ada
Elias Mårtenson <lokedhs@gmail.com>
2013-10-29 15:21:25 +0800

    Print an error message if the tool is started without arguments

--- fw_analyser/src/com/murex/fw/Main.java
+++ fw_analyser/src/com/murex/fw/Main.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 public class Main
 {
     public static void main( String[] args ) {
+        if( args.length == 0 ) {
+            usage();
+        }
+
         String type = args[0];
 
         if( type.equals( "s" ) ) {
